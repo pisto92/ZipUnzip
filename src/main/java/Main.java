514adc4f097e8ee1248937b465824eefdb5e264f
andre.pisto <pisto9228@gmail.com>
2018-06-20 11:51:59 +0200

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        String sourcePath = "C:\\Users\\andre.pisto\\Desktop\\FolderPre";
-        String destinationZipFile = "C:\\Users\\andre.pisto\\Desktop\\folderZipped.zip";
-        String destinationPath = "C:\\Users\\andre.pisto\\Desktop\\unzipped";
+        String sourcePath = "file or folder sopurce path";
+        String destinationZipFile = "destination path for zip file";
+        String destinationPath = "destination path for unzipped file";
 
         ZipUtils zipUtils = new ZipUtils();
         try {
